79e4919c4dba923b237be93f70ba3e1718e1a816
dnjzm108 <dnjzm108@kakao.com>
2021-05-13 18:08:21 +0900

    test3

---
+++ product.sql
@@ -0,0 +1,17 @@
+create database amazon;
+
+use amazon;
+
+create table product(
+category ,
+product_name ,
+price ,
+info ,
+product_image ,
+hit ,
+);
+
+insert into product(category,product_name,price,info,product_image,hit
+) values(
+
+);
