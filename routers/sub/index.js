b883542e31d0e21064d5743969fe56fafd9d3425
simbian2 <simbianartist@gmail.com>
2021-05-14 10:17:42 +0900

    seoyoung

--- routers/sub/index.js
+++ routers/sub/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./sub_controller')
 
-router.use('/',controller.sub);
+router.get('/todaysdeal',controller.today);
+router.get('/customerservice',controller.customer);
+router.get('/giftcards',controller.giftcards);
+router.get('/registry',controller.registry);
+router.get('/sell',controller.sell);
 
 module.exports = router;
